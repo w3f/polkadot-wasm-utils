91556df7f5530bf66617a5575e2c787677f58224
Radha <86818441+DrW3RK@users.noreply.github.com>
2024-11-02 18:58:31 +0100

    Polkadot Utils - Vesting
    Unlock vested DOT

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use routes::vesting::VestingComponent;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+mod routes;
+mod services;
+
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+pub enum Route {
+    #[at("/vesting")]
+    Vesting,
+    #[not_found]
+    #[at("/")]
+    Home,
+}
+
+fn main() {
+    yew::Renderer::<VestingApp>::new().render();
+}
+
+struct VestingApp;
+
+impl Component for VestingApp {
+    type Message = ();
+
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        VestingApp
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <BrowserRouter>
+                <Switch<Route> render={switch} />
+            </BrowserRouter>
+        }
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Vesting => html! { <VestingComponent/> },
+        Route::Home => {
+            html! {
+            <div>
+                <h1>{"Polkadot Utilities by W3F TechEd team"}</h1>
+                <a href="/vesting"> <button>{"Unlock vested balance"}</button></a>
+            </div> }
+        }
+    }
+}
