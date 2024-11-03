0c77cf4b3c9a43aaecf8a30a319d310a9f614371
Radha <86818441+DrW3RK@users.noreply.github.com>
2024-11-03 04:57:49 +0100

    cleaner UI

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,79 @@ mod services;
 pub enum Route {
     #[at("/vesting")]
     Vesting,
-    #[not_found]
     #[at("/")]
     Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
+
+mod pages {
+    use super::*;
+
+    #[function_component(Home)]
+    pub fn home() -> Html {
+        html! {
+            <div>  
+                <h1>{"Polkadot Utilities by W3F TechEd team"}</h1>
+                <HashRouter>
+                    <Link<Route> to={Route::Vesting}>
+                        <a> <button>{"Unlock vested balance"}</button></a>
+                    </Link<Route>>
+                </HashRouter>
+            </div> 
+        }
+    }
+
+    #[function_component(Vesting)]
+    pub fn vesting() -> Html {
+         html! { <VestingComponent/> }
+    }
+
+    #[function_component(NotFound)]
+    pub fn not_found() -> Html {
+        html! {
+            <div class="container">
+                <h1 class="title">{ "404" }</h1>
+                <p>{ "This is the 404 page." }</p>
+            </div>
+        }
+    }
+}
+
 
 fn main() {
     yew::Renderer::<VestingApp>::new().render();
 }
 
-struct VestingApp;
+#[function_component(VestingApp)]
+pub fn vesting_app() -> Html {
+    html! {
+        <HashRouter>
+            <div class="navbar">
+                <Link<Route> classes={classes!("button")} to={Route::Home}>
+                    { "Home" }
+                </Link<Route>>
+                { " | " }
+                <Link<Route> classes={classes!("button")} to={Route::Vesting}>
+                    { "Vesting" }
+                </Link<Route>>
+            </div>
 
-impl Component for VestingApp {
-    type Message = ();
-
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        VestingApp
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <BrowserRouter>
+            <main>
                 <Switch<Route> render={switch} />
-            </BrowserRouter>
-        }
+            </main>
+            <footer class="footer">
+            </footer>
+        </HashRouter>
     }
 }
 
 fn switch(routes: Route) -> Html {
+
     match routes {
-        Route::Vesting => html! { <VestingComponent/> },
-        Route::Home => html! { <VestingComponent/> }
+        Route::Home => html! { <pages::Home /> },
+        Route::Vesting => html! { <pages::Vesting /> },
+        Route::NotFound => html! { <pages::NotFound /> },
     }
 }
